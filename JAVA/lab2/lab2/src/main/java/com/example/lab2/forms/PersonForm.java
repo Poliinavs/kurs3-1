85c7e49d8aacad19fb51fc648872e5218587c68c
Polina <polina.avs@gmail.com>
2023-09-26 11:30:17 +0300

    'added'

---
+++ JAVA/lab2/lab2/src/main/java/com/example/lab2/forms/PersonForm.java
@@ -0,0 +1,13 @@
+package com.example.lab2.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonForm {
+    private String surname;
+    private String firstname;
+}
---
+++ JAVA/lab2/lab2/src/main/java/com/example/lab2/model/Person.java
@@ -0,0 +1,13 @@
+package com.example.lab2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+}
